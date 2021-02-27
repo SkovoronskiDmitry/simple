3817ab684e53302ff793142eb393cb8fdc7d0e52
dmitry <dmitryskovoronski@gmail.com>
2021-02-27 20:55:06 +0300

    add index.html

--- src/main/java/com/mastery/java/task/simple/config/SimpleApplication.java
+++ src/main/java/com/mastery/java/task/simple/config/SimpleApplication.java
@@ -22,35 +22,7 @@ public class SimpleApplication {
 
         ApplicationContext context = SpringApplication.run(SimpleApplication.class, args);
 
-        EmployeeServiceImp employeeServiceImp = context.getBean(EmployeeServiceImp.class);
 
-        System.out.println(employeeServiceImp.findAll());
-
-        System.out.println();
-
-        System.out.println(employeeServiceImp.findById(3));
-
-        Employee employee = new Employee();
-        employee.setFirstName("Gena");
-        employee.setLastName("Na");
-        employee.setJobTitle("engine");
-        employee.setDepartmentId(7l);
-        employee.setDateOfBirth(LocalDate.now());
-        employee.setGender("MALE");
-        System.out.println("CREATE:"+employeeServiceImp.createEmployee(employee));
-        System.out.println("created employee: "+employeeServiceImp.findAll());
-
-        Employee employee2 = new Employee();
-        employee2.setEmployeeId(1L);
-        employee2.setFirstName("Gena");
-        employee2.setLastName("Na");
-        employee2.setJobTitle("developer");
-        employee2.setDepartmentId(12_453l);
-        employee2.setDateOfBirth(LocalDate.now());
-        employee2.setGender("MALE");
-
-        employeeServiceImp.updateEmployee(employee2);
-        System.out.println("update Employee: "+employeeServiceImp.findAll());
 
 
 
