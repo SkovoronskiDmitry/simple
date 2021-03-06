17be5885e363ac91ae60795cc281df399b0386dc
dmitry <dmitryskovoronski@gmail.com>
2021-03-06 19:14:55 +0300

    Logger

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ VALUES ('Ivan', 'Ivanov',7,'boss','male','1970-01-10');
 INSERT INTO employee (first_name, last_name ,department_id, job_title, gender,date_of_birth)
 VALUES ('Petr', 'Petrov',6,'employee','male','1990-05-10');
 INSERT INTO employee (first_name, last_name ,department_id, job_title, gender,date_of_birth)
-VALUES ('Vers', 'Ivanova',7,'secretary','female','2000-06-10');
+VALUES ('Vera', 'Ivanova',7,'secretary','female','2000-06-10');
