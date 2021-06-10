211eda5d470bf77d74baa2774cc104d4e2902b3d
dmitry <dmitryskovoronski@gmail.com>
2021-06-10 16:46:18 +0300

    add exclude in SpringBootApplication

--- src/main/java/com/mastery/java/task/simple/SimpleApplication.java
+++ src/main/java/com/mastery/java/task/simple/SimpleApplication.java
@@ -2,13 +2,22 @@ package com.mastery.java.task.simple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        ThymeleafAutoConfiguration.class,
+        TaskSchedulingAutoConfiguration.class,
+        SpringDataWebAutoConfiguration.class,
+        SecurityAutoConfiguration.class
+})
 @EnableSwagger2
 public class SimpleApplication {
 
