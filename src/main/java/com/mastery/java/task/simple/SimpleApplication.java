cd0cd8c2fa345c4db6d4a698d4ff9886f00729b3
dmitry <dmitryskovoronski@gmail.com>
2021-07-08 23:40:43 +0300

    add JMS

--- src/main/java/com/mastery/java/task/simple/SimpleApplication.java
+++ src/main/java/com/mastery/java/task/simple/SimpleApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -15,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
         SecurityAutoConfiguration.class
 })
 @EnableSwagger2
+@EnableJms
 public class SimpleApplication {
 
     @Bean
